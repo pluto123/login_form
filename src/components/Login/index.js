cde87004c5ae362935f88c4ad99ad9ac64110b09
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-12-31 22:57:10 +0800

    完成 Login 外框

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,17 @@
 import { Grid, Paper } from '@material-ui/core'
 
 const Login=()=>{
+    const paperStyle = {
+        padding: 20, 
+        height: '70vh',
+        width: 200,
+        margin: '20px auto'
+    }
     return (
         <Grid>
             <Paper 
                 elevation={10} // 讓此組件隆起，產生陰影效果
+                style={paperStyle} // 設定 Paper 組件樣式
             >
                 Sign in
             </Paper>
