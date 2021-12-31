dd9ee9e5fa36ceab8ecd3b43f628ef6adcac7972
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2021-12-31 20:44:30 +0800

    use elevation

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,14 @@
+import { Grid, Paper } from '@material-ui/core'
 
 const Login=()=>{
     return (
-        "login"
+        <Grid>
+            <Paper 
+                elevation={10} // 讓此組件隆起，產生陰影效果
+            >
+                Sign in
+            </Paper>
+        </Grid>
     )
 }
 
