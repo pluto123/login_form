ac74ff09bb90f4c69dbf8d6d3f82e2f613051ada
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2022-01-01 10:49:37 +0800

    TextField

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,4 @@
-import { Grid, Paper, Avatar } from '@material-ui/core'
+import { Grid, Paper, Avatar, TextField } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 const Login=()=>{
@@ -12,13 +12,17 @@ const Login=()=>{
         backgroundColor: 'green'
     }
     return (
-        <Grid align='center'> {/* 將物件置中 */}
+        <Grid>
             <Paper 
                 elevation={10} // 讓此組件隆起，產生陰影效果
                 style={paperStyle} // 設定 Paper 組件樣式
             >
-                <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
-                <h2>Sign in</h2>
+                <Grid align='center'> {/* 將物件置中 */}
+                    <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
+                    <h2>Sign in</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
             </Paper>
         </Grid>
     )
